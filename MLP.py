b417371e6813d28a4ddafc2ad771482138c5dc82
kautharorrie <orrkau001@myuct.ac.za>
2023-04-24 13:49:52 +0200

    created a transfrom sequence.

--- MLP.py
+++ MLP.py
@@ -2,5 +2,15 @@ import torch  # Main Package
 import torchvision  # Package for Vision Related ML
 import torchvision.transforms as transforms  # Subpackage that contains image transforms
 
-
+# Create the transform sequence
+# container that contains a sequence of transform sequence
+# transforms the image to tensors
+# data needs to be transfomed to a tensor
+transform = transforms.Compose([
+    transforms.ToTensor(),  # Convert to Tensor
+    # used 3 values for the CIFA10 dataset because it is RGB
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)) 
+])
+#normalise is to change the value of pixel to 1 or -1
+#for assignment we can chnage the colours or normalise them with different mean and deviatoion
 
