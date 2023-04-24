8985f81df5078d7f9870fd41a6d45e652a407bb0
kautharorrie <orrkau001@myuct.ac.za>
2023-04-24 13:56:33 +0200

    Added the code where the CIFAR10 dataset is automatically downloaded using torchvision

--- MLP.py
+++ MLP.py
@@ -8,9 +8,24 @@ import torchvision.transforms as transforms  # Subpackage that contains image tr
 # data needs to be transfomed to a tensor
 transform = transforms.Compose([
     transforms.ToTensor(),  # Convert to Tensor
-    # used 3 values for the CIFA10 dataset because it is RGB
+    # used 3 values for the CIFAR10 dataset because it is RGB
     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)) 
 ])
 #normalise is to change the value of pixel to 1 or -1
 #for assignment we can chnage the colours or normalise them with different mean and deviatoion
+
+# Load CIFAR-10 dataset
+# download the dataset using torch vision
+
+# Train
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+# Test
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+# the batch size you want to train on at a time
+# Send data to the data loaders
+BATCH_SIZE = 128
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False)
 
