c46117d6798577748d3d228614fbc86ad37bc54e
kautharorrie <orrkau001@myuct.ac.za>
2023-04-27 21:52:08 +0200

    added the necessary imports

---
+++ RESNET.py
@@ -0,0 +1,9 @@
+import torch  # Main Package
+import torchvision  # Package for Vision Related ML
+import torchvision.transforms as transforms  # Subpackage that contains image transforms
+
+import torch.nn as nn  # Layers
+import torch.nn.functional as F # Activation Functions
+
+import torch.optim as optim # Optimizers
+
