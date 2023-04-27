ee9032aa80fcf0ec69cb229e2fdb05dbaf834eda
kautharorrie <orrkau001@myuct.ac.za>
2023-04-27 21:54:56 +0200

    added the test and train functions - same as CNN.py

--- RESNET.py
+++ RESNET.py
@@ -7,3 +7,67 @@ import torch.nn.functional as F # Activation Functions
 
 import torch.optim as optim # Optimizers
 
+# Create the transform sequence
+# container that contains a sequence of transform sequence
+# transforms the image to tensors
+# data needs to be transfomed to a tensor
+transform = transforms.Compose([
+    transforms.ToTensor(),  # Convert to Tensor
+    # used 3 values for the CIFAR10 dataset because it is RGB
+    # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)) 
+])
+#normalise is to change the value of pixel to 1 or -1
+#for assignment we can chnage the colours or normalise them with different mean and deviatoion
+
+# Load CIFAR-10 dataset
+# download the dataset using torch vision
+
+# Train
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+# Test
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+# the batch size you want to train on at a time
+# Send data to the data loaders
+BATCH_SIZE = 500
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False)
+
+# outside of MLP class
+# send all the parameters or output to the right place, this code checks which device you have
+# Identify device
+device = ("cuda" if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+# Define the training and testing functions
+def train(net, train_loader, criterion, optimizer, device):
+    net.train()  # Set model to training mode.
+    running_loss = 0.0  # To calculate loss across the batches
+    for data in train_loader:
+        inputs, labels = data  # Get input and labels for batch
+        inputs, labels = inputs.to(device), labels.to(device)  # Send to device
+        optimizer.zero_grad()  # Zero out the gradients of the ntwork i.e. reset
+        outputs = net(inputs)  # Get predictions
+        loss = criterion(outputs, labels)  # Calculate loss
+        loss.backward()  # Propagate loss backwards
+        optimizer.step()  # Update weights
+        running_loss += loss.item()  # Update loss
+    return running_loss / len(train_loader)
+
+def test(net, test_loader, device):
+    net.eval()  # We are in evaluation mode
+    correct = 0
+    total = 0
+    with torch.no_grad():  # Don't accumulate gradients
+        for data in test_loader:
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device) # Send to device
+            outputs = net(inputs)  # Get predictions
+            _, predicted = torch.max(outputs.data, 1)  # Get max value
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()  # How many are correct?
+    return correct / total
